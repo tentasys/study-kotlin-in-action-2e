4a052f403ca34bfa34b0c54d7175cb97d14ce1fd
michol.lee <lovethefeel@gmail.com>
2025-04-14 00:50:20 +0900

    refactor: 로또 번호 리팩토링

--- 이상호/practice/kotlin-lotto-study/src/main/kotlin/io/study/kotlin/lotto/Number.kt
+++ 이상호/practice/kotlin-lotto-study/src/main/kotlin/io/study/kotlin/lotto/Number.kt
@@ -1,13 +1,35 @@
 package io.study.kotlin.lotto
 
-class Number(private val number : Int) {
+open class Number(private var _number : Int) {
+
+    val number: Int
+        get() = _number
+
+    // 내부에서만 값을 변경하는 메서드 제공
+    fun setValue(number: Int) {
+        _number = number
+    }
+
     init {
-        require(number in 1..45) {
-            throw IllegalArgumentException("Value must be between 1 and 45")
+        require(_number in 1..45) {
+            throw IllegalArgumentException("로또 숫자는 1 ~ 45 사이여야 한다.")
         }
     }
 
     override fun toString(): String {
-        return "$number"
+        return "$_number"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Number
+
+        return _number == other._number
+    }
+
+    override fun hashCode(): Int {
+        return _number
     }
 }
